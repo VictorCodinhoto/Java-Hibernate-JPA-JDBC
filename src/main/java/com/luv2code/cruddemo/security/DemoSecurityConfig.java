431fcf0866ae6497e482485f2602af7fea3d911e
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-09-03 16:55:34 -0300

    Getting Started with Secure

---
+++ src/main/java/com/luv2code/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.luv2code.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails victor = User.builder()
+                .username("victor")
+                .password("{noop}codinhoto")
+                .roles("Employee","Manager","Administrator")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("Mary")
+                .password("{noop}Ane123")
+                .roles("Employee")
+                .build();
+
+        return  new InMemoryUserDetailsManager(victor,mary);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMINISTRATOR")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("MANAGER"));
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
